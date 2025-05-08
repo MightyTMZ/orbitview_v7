66be8e53cd81ef54d6781487e79722c25314b27b
Tom Zhang <tommyawesome08@gmail.com>
2025-05-08 17:33:16 -0400

    user creation serializers fields updated

--- orbitview/users/serializers.py
+++ orbitview/users/serializers.py
@@ -4,10 +4,31 @@ from .models import CustomUser
 class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name', 'role')
+        fields = (
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'bio',
+            'website',
+            'profile_image',
+        )
+
+        read_only_fields = ['id', 'username', 'email']
+
 
 
 class CustomUserSerializer(UserSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'role', 'bio', 'website', 'profile_image')
+        fields = (
+            'id', 
+            'username', 
+            'email', 
+            'first_name', 
+            'last_name', 
+            'bio', 
+            'website', 
+            'profile_image'
+        )
